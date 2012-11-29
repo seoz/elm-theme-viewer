ac6ec269ac92581d0b86e66a1c2b2aee0ea4065d
Daniel Juyung Seo <seojuyung@gmail.com>
2012-11-30 02:37:24 +0900

    use box for the contents instead of panes.

--- gui.c
+++ gui.c
@@ -2,7 +2,42 @@
 #include "log.h"
 #include "theme.h"
 
+#define WEIGHT evas_object_size_hint_weight_set
+#define ALIGN_ evas_object_size_hint_align_set
+#define EXPAND(X) WEIGHT((X), EVAS_HINT_EXPAND, EVAS_HINT_EXPAND)
+#define FILL(X) ALIGN_((X), EVAS_HINT_FILL, EVAS_HINT_FILL)
+
 Evas_Object *list;
+
+static Evas_Object *
+_elm_min_set(Evas_Object *obj, Evas_Object *parent, Evas_Coord w, Evas_Coord h)
+{
+   Evas_Object *table, *rect;
+
+   table = elm_table_add(parent);
+   evas_object_show(table);
+
+   rect = evas_object_rectangle_add(evas_object_evas_get(table));
+   evas_object_size_hint_min_set(rect, w, h);
+   EXPAND(rect); FILL(rect);
+   elm_table_pack(table, rect, 0, 0, 1, 1);
+
+   EXPAND(obj); FILL(obj);
+   elm_table_pack(table, obj, 0, 0, 1, 1);
+
+   return table;
+}
+
+static Evas_Object *
+_right_content_create(Evas_Object *parent)
+{
+   Evas_Object *o;
+
+   o = elm_bg_add(parent);
+   evas_object_show(o);
+
+   return o;
+}
 
 static Evas_Object *
 _left_menu_create(Evas_Object *parent)
@@ -24,7 +59,7 @@ void
 gui_create(const char *edje_file)
 {
    Evas_Object *win, *o;
-   Evas_Object *box, *lbl, *btn, *panes, *table, *left;
+   Evas_Object *box, *lbl, *btn, *box2, *table, *left, *right;
 
    if (!edje_file) return;
 
@@ -35,6 +70,7 @@ gui_create(const char *edje_file)
    evas_object_resize(o, 300, 400);
    evas_object_show(o);
 
+   // outer box
    box = o = elm_box_add(win);
    evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    elm_win_resize_object_add(win, o);
@@ -52,17 +88,23 @@ gui_create(const char *edje_file)
    elm_box_pack_end(box, o);
    evas_object_show(o);
 
-   panes = o = elm_panes_add(win);
+   // inner box
+   box2 = o = elm_box_add(win);
+   elm_box_horizontal_set(o, EINA_TRUE);
    evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(o, EVAS_HINT_FILL, EVAS_HINT_FILL);
    elm_box_pack_end(box, o);
    evas_object_show(o);
 
-   left = o = _left_menu_create(win);
-   evas_object_size_hint_weight_set(o, 0.0, EVAS_HINT_EXPAND);
-   evas_object_size_hint_align_set(o, EVAS_HINT_FILL, EVAS_HINT_FILL);
-   elm_object_part_content_set(panes, "left", o);
-   evas_object_show(o);
+   left = _left_menu_create(win);
+   table = o = _elm_min_set(left, box, 200, 0); // set min size hint
+   WEIGHT(o, 0.0, EVAS_HINT_EXPAND);
+   FILL(o);
+   elm_box_pack_end(box2, table);
+
+   right = o = _right_content_create(win);
+   EXPAND(o); FILL(o);
+   elm_box_pack_end(box2, o);
 
    INF("GUI Creation Done");
 
