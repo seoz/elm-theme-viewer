f95b30f8e7e61d24c74a4b109a93eb807e4e1fcf
Daniel Juyung Seo <seojuyung2@gmail.com>
2012-11-30 15:02:39 +0900

    added missing common.h

---
+++ common.h
@@ -0,0 +1,9 @@
+#ifndef __INCLUDE_COMMON_
+#define __INCLUDE_COMMON_
+
+#define WEIGHT evas_object_size_hint_weight_set
+#define ALIGN evas_object_size_hint_align_set
+#define EXPAND(X) WEIGHT((X), EVAS_HINT_EXPAND, EVAS_HINT_EXPAND)
+#define FILL(X) ALIGN((X), EVAS_HINT_FILL, EVAS_HINT_FILL)
+
+#endif
