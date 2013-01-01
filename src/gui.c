d16c672212d929af1757534fbb60a7f4f3418c3e
Daniel Juyung Seo <seojuyung@gmail.com>
2013-01-02 01:57:58 +0900

    use elm_layout for elm_theme_viewer layout.

--- src/gui.c
+++ src/gui.c
@@ -4,7 +4,7 @@
 #include "theme.h"
 #include "widget.h"
 
-Evas_Object *list, *win, *viewer_box, *viewer_box_cur_obj;
+Evas_Object *list, *win, *gui_layout, *preview_obj;
 
 typedef struct _Style_Data Style_Data;
 struct _Style_Data
@@ -14,42 +14,23 @@ struct _Style_Data
 };
 
 static void
-_viewer_box_obj_add(const char *widget, const char*style)
+_preview_create(const char *widget, const char*style)
 {
    Evas_Object *o;
 
    o = widget_create(widget, style);
-   elm_box_pack_end(viewer_box, o);
-   viewer_box_cur_obj = o;
+   elm_layout_content_set(gui_layout, "preview", o);
+   preview_obj = o;
 }
 
 static void
-_viewer_box_obj_del(void)
+_preview_del(void)
 {
-   evas_object_del(viewer_box_cur_obj);
-   viewer_box_cur_obj = NULL;
+   evas_object_del(preview_obj);
+   preview_obj = NULL;
 }
 
-static Evas_Object *
-_elm_min_set(Evas_Object *obj, Evas_Object *parent, Evas_Coord w, Evas_Coord h)
-{
-   Evas_Object *table, *rect;
-
-   table = elm_table_add(parent);
-   evas_object_show(table);
-
-   rect = evas_object_rectangle_add(evas_object_evas_get(table));
-   evas_object_size_hint_min_set(rect, w, h);
-   EXPAND(rect); FILL(rect);
-   elm_table_pack(table, rect, 0, 0, 1, 1);
-
-   EXPAND(obj); FILL(obj);
-   elm_table_pack(table, obj, 0, 0, 1, 1);
-
-   return table;
-}
-
-static Evas_Object *
+static void
 _left_menu_create(Evas_Object *parent)
 {
    Evas_Object *nf;
@@ -63,14 +44,13 @@ _left_menu_create(Evas_Object *parent)
    elm_naviframe_item_push(nf, "Widgets", NULL, NULL, list, NULL);
    evas_object_show(list);
 
-   return nf;
+   elm_layout_content_set(gui_layout, "left_menu", nf);
 }
 
 void
 gui_create(const char *edje_file)
 {
    Evas_Object *o;
-   Evas_Object *box, *btn, *table, *left;
 
    if (!edje_file) return;
 
@@ -81,40 +61,20 @@ gui_create(const char *edje_file)
    evas_object_resize(o, 600, 500);
    evas_object_show(o);
 
-   // outer box
-   box = o = elm_box_add(win);
+   gui_layout = o = elm_layout_add(win);
+   elm_layout_file_set(o, "data/layout.edj", "etv/main/layout");
    evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    elm_win_resize_object_add(win, o);
    evas_object_show(o);
 
-   o = elm_label_add(win);
-   elm_object_text_set(o, edje_file);
-   evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, 0.0);
-   elm_box_pack_end(box, o);
-   evas_object_show(o);
+   // title text
+   elm_layout_text_set(gui_layout, "edje_name_txt", edje_file);
 
-   btn = o = elm_button_add(win);
-   elm_object_disabled_set(btn, EINA_TRUE);
-   elm_object_text_set(o, "Select Theme");
-   evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, 0.0);
-   elm_box_pack_end(box, o);
-   evas_object_show(o);
+   // left menu
+   _left_menu_create(win);
 
-   // inner box
-   viewer_box = o = elm_box_add(win);
-   elm_box_horizontal_set(o, EINA_TRUE);
-   evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   evas_object_size_hint_align_set(o, EVAS_HINT_FILL, EVAS_HINT_FILL);
-   elm_box_pack_end(box, o);
-   evas_object_show(o);
-
-   left = _left_menu_create(win);
-   table = o = _elm_min_set(left, box, 250, 0); // set min size hint
-   WEIGHT(o, 0.0, EVAS_HINT_EXPAND);
-   FILL(o);
-   elm_box_pack_end(viewer_box, table);
-
-   _viewer_box_obj_add(NULL, NULL);
+   // preview
+   _preview_create(NULL, NULL);
 
    INF("GUI Creation Done");
 
@@ -129,17 +89,17 @@ _style_list_sel_cb(void *data, Evas_Object *obj, void *event_info)
    if (!data || !sd->widget || !sd->style) return;
    INF("%s %s", sd->widget, sd->style);
 
-   _viewer_box_obj_del();
-   _viewer_box_obj_add(sd->widget, sd->style);
+   _preview_del();
+   _preview_create(sd->widget, sd->style);
 }
 
 static void
 _nf_prev_btn_clicked_cb(void *data, Evas_Object *obj, void *event_info)
 {
-   if (viewer_box_cur_obj)
+   if (preview_obj)
      {
-        _viewer_box_obj_del();
-        _viewer_box_obj_add(NULL, NULL);
+        _preview_del();
+        _preview_create(NULL, NULL);
      }
 
    elm_naviframe_item_pop(data);
@@ -149,16 +109,16 @@ static void
 _style_list_gengrid_group_index_sel_cb(void *data, Evas_Object *obj,
                                       void *event_info)
 {
-   _viewer_box_obj_del();
-   _viewer_box_obj_add("gengrid", "h9 group-index-style");
+   _preview_del();
+   _preview_create("gengrid", "h9 group-index-style");
 }
 
 static void
 _style_list_gengrid_grid_check_sel_cb(void *data, Evas_Object *obj,
                                       void *event_info)
 {
-   _viewer_box_obj_del();
-   _viewer_box_obj_add("gengrid", "h9 grid-check-style");
+   _preview_del();
+   _preview_create("gengrid", "h9 grid-check-style");
 }
 
 static void
