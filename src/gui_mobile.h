2b7c1e044293d7f45deb3d72987f4f3ee72a3e73
Daniel Juyung Seo <juyung.seo@samsung.com>
2013-01-31 15:08:55 +0900

    gui: use separate macro for gui_mobile.h include.
    fixed the following warnings.
    main.c:97:9: warning: implicit declaration of function ‘gui_mobile_create’ [-Wimplicit-function-declaration]
    main.c:99:9: warning: implicit declaration of function ‘gui_mobile_widget_load’ [-Wimplicit-function-declaration]

--- src/gui_mobile.h
+++ src/gui_mobile.h
@@ -1,5 +1,5 @@
-#ifndef __INCLUDE_GUI__
-#define __INCLUDE_GUI__
+#ifndef __INCLUDE_GUI_MOBILE__
+#define __INCLUDE_GUI_MOBILE__
 
 void gui_mobile_create(const char *edje_file, int width, int height);
 void gui_mobile_widget_load(void);
